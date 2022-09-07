65c2c27cae5b227ece1ddf68592b5fbc39da1342
Sobinjm <jmsobin@gmail.com>
2022-09-07 15:56:16 +0530

    initial commit

---
+++ src/app/features/auth/login/login.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+
+  public newUser = false;
+  // public user: firebase.User;
+  public loginForm: FormGroup;
+  public formErrors: {email:any,password:any} = {
+    'email': '',
+    'password': '',
+  };
+  public errorMessage: any;
+
+  constructor( private fb: FormBuilder,
+    private router: Router) {
+    this.loginForm = fb.group({
+      email: ['test@gmail.com', [Validators.required, Validators.email]],
+      password: ['test123', Validators.required]
+    });
+  }
+
+  ngOnInit() {
+  }
+
+
+
+
+
+}
