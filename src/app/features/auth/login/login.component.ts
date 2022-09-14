51454c75b0364e4711e7e4fe55cd3d1c580cde14
celiba <celibanarayanan@gmail.com>
2022-09-14 16:53:34 +0530

    login_form

--- src/app/features/auth/login/login.component.ts
+++ src/app/features/auth/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, FormArray, Validators,FormBuilder } from '@angular/forms';
+import { FormGroup, FormControl, FormArray, Validators, FormBuilder } from '@angular/forms';
 import { Router } from '@angular/router';
 import Swal from 'sweetalert2';
 import { NgModule } from '@angular/core';
@@ -11,53 +11,29 @@ import { NgModule } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-
-  public newUser = false;
-  // public user: firebase.User;
-  loginForm!: FormGroup;
-  submitted = false;
-  
-  public formErrors: {email:any,password:any} = {
-    'email': '',
-    'password': '',
-  };
-  public errorMessage: any;
-
-  
-  constructor( private fb: FormBuilder,
+  login_Form!: FormGroup;
+  constructor(private fb: FormBuilder,
     private router: Router) {
-   
-
   }
-  get logincontrol() {
-   return this.loginForm.controls;
-    }
+  get login_control() {
+    return this.login_Form.controls;
+  }
   ngOnInit() {
-    this.loginForm = this.fb.group({
-      email: [null, [Validators.required, Validators.email,Validators.pattern("^((\\+91-?)|0)?[0-9]{10}$")]],
+    this.login_Form = this.fb.group({
+      user_id: [null, [Validators.required, Validators.pattern(/^(\d{10}|\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3}))$/)]],
       password: [null, [Validators.required, Validators.minLength(6)]]
     });
-    
   }
-
-  loginSubmit() {
-   
-
-   if (this.loginForm.invalid) {
-    this.submitted = true;
- 
-    Swal.fire('Invalid User');
-    }
-    else{
-      
+  login_Submit() {
+    if (this.login_Form.valid) {
       Swal.fire('success');
-
-   
     }
-   }
-
+    else {
+      Swal.fire('Invalid User');
     }
-  
+  }
+}
 
-  
+
+
 
