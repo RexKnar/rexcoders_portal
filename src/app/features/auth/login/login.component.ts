c7fd4fce228d3daf938f8180eabf54aaae22d432
AbarnaRM <abarna.rm@rexknar.com>
2022-09-15 16:24:21 +0530

    Update login.component.ts

--- src/app/features/auth/login/login.component.ts
+++ src/app/features/auth/login/login.component.ts
@@ -1,38 +1,3 @@
-// import { Component, OnInit } from '@angular/core';
-// import { FormGroup, FormControl, FormArray, Validators, FormBuilder } from '@angular/forms';
-// import { Router } from '@angular/router';
-// import Swal from 'sweetalert2';
-
-// @Component({
-//   selector: 'app-login',
-//   templateUrl: './login.component.html',
-//   styleUrls: ['./login.component.scss']
-// })
-// export class LoginComponent implements OnInit {
-//   login_Form!: FormGroup;
-//   constructor(private fb: FormBuilder,
-//     private router: Router) {
-//   }
-  
-//   get login_control() {
-//     return this.login_Form.controls;
-//   }
-//   ngOnInit() {
-//     this.login_Form = this.fb.group({
-//       user_id: [null, [Validators.required, Validators.pattern(/^(\d{10}|\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3}))$/)]],
-//       password: [null, [Validators.required, Validators.minLength(6)]]
-//     });
-//   }
-//   loginSubmit() {
-//     if (this.login_Form.valid) {
-//       Swal.fire('success');
-//     }
-//     else {
-//       Swal.fire('Invalid User');
-//     }
-//   }
-// }
-
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
 import { Router } from '@angular/router';
@@ -53,7 +18,7 @@ export class LoginComponent implements OnInit {
 
     private router: Router) {
   }
-  get login_control() {
+  get loginControl() {
     return this.loginForm.controls;
   }
   ngOnInit() {
