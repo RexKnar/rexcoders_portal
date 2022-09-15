62262698ad1b84b5e9f83626d6b366acf63935c9
Sobinjm <jmsobin@gmail.com>
2022-09-15 15:47:03 +0530

    Refactor

--- src/app/features/auth/login/login.component.ts
+++ src/app/features/auth/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, FormArray, Validators, FormBuilder } from '@angular/forms';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
 import { Router } from '@angular/router';
 import Swal from 'sweetalert2';
 
@@ -9,21 +9,21 @@ import Swal from 'sweetalert2';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-  login_Form!: FormGroup;
-  constructor(private fb: FormBuilder,
+  loginForm!: FormGroup;
+  constructor(private loginFormBuilder: FormBuilder,
     private router: Router) {
   }
   get login_control() {
-    return this.login_Form.controls;
+    return this.loginForm.controls;
   }
   ngOnInit() {
-    this.login_Form = this.fb.group({
-      user_id: [null, [Validators.required, Validators.pattern(/^(\d{10}|\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3}))$/)]],
+    this.loginForm = this.loginFormBuilder.group({
+      userId: [null, [Validators.required, Validators.email],
       password: [null, [Validators.required, Validators.minLength(6)]]
     });
   }
   loginSubmit() {
-    if (this.login_Form.valid) {
+    if (this.loginForm.valid) {
       Swal.fire('success');
     }
     else {
