0903c2fb95e03534d543611d2151579056376c0b
sheikali85 <sheikali85@gmail.com>
2022-10-03 11:49:50 +0530

    Sidebar Updates 03-10-2022 Batch-2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HttpClientModule } from '@angular/common/http';
+
 
 import { SharedModule } from './shared/shared.module';
 import {CookieService} from 'ngx-cookie-service';
