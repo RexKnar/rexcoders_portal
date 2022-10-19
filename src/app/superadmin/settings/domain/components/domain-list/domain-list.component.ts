76d0b740a96af9eb822e831e8a095cab3bd46b84
sheikali85 <sheikali85@gmail.com>
2022-10-19 17:23:17 +0530

    Training Branch Updated on 19-10-2022 Batch-1

--- src/app/superadmin/settings/domain/components/domain-list/domain-list.component.ts
+++ src/app/superadmin/settings/domain/components/domain-list/domain-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, ViewChild,ElementRef} from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { AddDomainModel } from 'src/app/shared/model/domain.model';
 import { DomainService } from 'src/app/shared/service/domain.service';
 import Swal from 'sweetalert2';
 @Component({
@@ -8,51 +9,60 @@ import Swal from 'sweetalert2';
 })
 export class DomainListComponent implements OnInit {
   domainlist: any;
-  domainName:string="";
-  domainData: { domainName:string };
-  @ViewChild('domainID') domainID:ElementRef;
-  index:number;
-  dId:number;
+  domainData: AddDomainModel= new AddDomainModel();
+  isAddDomain:boolean;
+  isEditDomain:boolean;
   constructor(private _domainService: DomainService) { }
 
+resetAddModal(){
+  this.isAddDomain=true; 
+  this.isEditDomain=false; 
+  this.domainData=new AddDomainModel();
+}
+
   ngOnInit(): void {
-      this._domainService.getDomain().subscribe((userRespose: any) => {
-      this.domainlist = userRespose;
-      this.index=this.domainlist.index;
-  });
+     this.getDomain();
+}
+getDomain(){
+  this._domainService.getDomain().subscribe((getDomainRespose: any) => {
+ this.domainlist = getDomainRespose;
+ this.domainData=this.domainlist;
+});
 }
 
   insertDomain()
   {
-    this._domainService.postDomain({'domainName':this.domainName}).subscribe((userRespose: any) => {
-    })
+    this._domainService.insertDomain(this.domainData).subscribe((postDomainRespose: any) => {
+    console.log(postDomainRespose);
     Swal.fire(
       'Good job!',
       'Domain name added!',
       'success'
     )
+    this.getDomain();
+    })
+  
   }
-  sendDomainId(domainDetails:any){
-    // console.log(domainDetails.domainId);
-    this.dId=domainDetails.domainId;
+  sendDomain(domainDetails:any){
+    this.domainData=domainDetails;
   }
   updateDomain()
   {
-    this._domainService.updateDomain({'domainName':this.domainName, 'domainId':this.dId}).subscribe((userRespose: any) => {
+    this._domainService.updateDomain(this.domainData).subscribe((updateDomainRespose: any) => {
+      console.log(updateDomainRespose);
+      Swal.fire(
+        '',
+        'Domain name updated successfully!',
+        'success'
+      )
     })
-    console.log(this.dId);
-    Swal.fire(
-      '',
-      'Domain name updated successfully!',
-      'success'
-    )
   }
   deleteDomain(domainDetails:any)
   {
     
     Swal.fire({
       title: 'Are you sure?',
-      text: "You won't be able to revert this!",
+      text: "Do you want to delete the Domain:"+domainDetails.domainName,
       icon: 'warning',
       showCancelButton: true,
       confirmButtonColor: '#3085d6',
@@ -61,13 +71,13 @@ export class DomainListComponent implements OnInit {
     }).then((result) => {
       if (result.isConfirmed) {
         this._domainService.deleteDomain(domainDetails.domainId).subscribe((userRespose: any) => {
-        console.log(domainDetails.domainId);
-      });
-        Swal.fire(
-          'Deleted!',
-          'Your domain has been deleted.',
-          'success'
-        )
+          Swal.fire(
+            'Deleted!',
+            'Your domain has been deleted.',
+            'success'
+          )
+        });
+       
       }
     })
   }
