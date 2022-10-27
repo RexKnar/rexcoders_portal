45f8004b78ee38224692985cee34b830030be11e
celiba <celibanarayanan@gmail.com>
2022-10-27 15:32:30 +0530

    interceptors

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule,HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HttpClientModule } from '@angular/common/http';
 
+import { AuthInterceptor } from './shared/Interceptors/auth.interceptor';
 import { SharedModule } from './shared/shared.module';
 import {CookieService} from 'ngx-cookie-service';
 @NgModule({
@@ -19,7 +19,9 @@ import {CookieService} from 'ngx-cookie-service';
     FormsModule,
     HttpClientModule,
   ],
-  providers: [CookieService],
+  providers: [{provide:HTTP_INTERCEPTORS,
+    useClass:AuthInterceptor,
+    multi:true},CookieService],
   schemas:[CUSTOM_ELEMENTS_SCHEMA],
   bootstrap: [AppComponent]
 })
