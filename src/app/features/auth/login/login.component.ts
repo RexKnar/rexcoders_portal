4b7a312ad1674cd7474a1e9adccb170597e2ec93
sheikali85 <sheikali85@gmail.com>
2022-11-22 10:45:34 +0530

    authmodule updated on 22-11-2022 batch-1

--- src/app/features/auth/login/login.component.ts
+++ src/app/features/auth/login/login.component.ts
@@ -47,7 +47,6 @@ export class LoginComponent implements OnInit, OnDestroy {
       this.loginSubscription=this._authservice.authenticateUser(this.loginForm.value).subscribe({
         next: (data) => {
           Swal.fire('Hi '+this.userRole + ', Welcome to Rexcoders');
-          console.log(this.loginForm.value);
           this.loginForm.reset();
       
        this.responsedata=data.data;
