9a026798562c18e7db6b760ce38336a1c030521a
sheikali85 <sheikali85@gmail.com>
2023-05-22 16:47:43 +0530

    local commit

--- src/app/features/superadmin/training/training-routing.module.ts
+++ src/app/features/superadmin/training/training-routing.module.ts
@@ -4,11 +4,6 @@ import { AddTrainingComponent } from './components/add-training/add-training.com
 import { ReviewComponent } from './components/review/review.component';
 import { TrainingListComponent } from './components/training-list/training-list.component';
 import { TrainingDetailsComponent } from './components/training-details/training-details.component';
-import { ViewTopicComponent } from './syllabus/components/syllabus/view-topic/view-topic.component';
-import { ModuleListComponent } from './syllabus/components/syllabus/module-list/module-list.component';
-import { ChapterListComponent } from './syllabus/components/syllabus/chapter-list/chapter-list.component';
-import { TopicListComponent } from './syllabus/components/syllabus/topic-list/topic-list.component';
-
 const routes: Routes = [
   {
     path: '',
@@ -28,29 +23,7 @@ const routes: Routes = [
   path:'review',
   component:ReviewComponent
 },
- {
-  path:'module-list',
-  component:ModuleListComponent
- },
- {
 
-  path:'topic-list',
-  component:TopicListComponent
- },
- {
- 
-  path:'chapter-list',
-  component:ChapterListComponent
- },
- {
-  path:'module-list',
-  component:ModuleListComponent
-
- },
- {
-  path:'view-topic',
-  component:ViewTopicComponent
- },
  {
   path:'training-list',
   component:TrainingDetailsComponent
