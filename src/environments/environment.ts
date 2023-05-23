24d7c08ecba6eaba84b8dc4c2758690e96ec1ccd
Arasu <malarasuk@gmail.com>
2023-05-23 12:34:06 +0530

    local commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 
 export const environment = {
   production: false,
-  // baseUrl:"https://development.rexcoders.in",
-  baseUrl:"http://localhost:7700",
+  baseUrl:"https://testing.rexcoders.in",
+  // baseUrl:"http://localhost:7700",
 
 }
